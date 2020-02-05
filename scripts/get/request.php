38a236eb45236af7b33805ada0c873628de19730
Adam Balsam <adam.balsam@acquia.com>
2020-02-04 21:07:23 -0500

    Add fixjson function withing request script

--- scripts/get/request.php
+++ scripts/get/request.php
@@ -1,16 +1,31 @@
 #!/usr/bin/env php
 <?php
 
-$endpoint = 'https://mayors24.cityofboston.gov/open311/v2/requests.json?service_code=4f389210e75084437f0001e5&jurisdiction_id=boston.gov&page=';
-$filename = 'responses--1999+.json';
-$i = 1999;
+$endpoint = 'https://mayors24.cityofboston.gov/open311/v2/requests.json?service_code=4f38920fe75084437f00018d&jurisdiction_id=boston.gov&page=';
+$filename = 'responses--sidewalk-not-shovelled--missing-407-412.json';
+$i = 407;
 
-while ($i < 2700) {
+while ($i < 413) {
     $contents = file_get_contents($endpoint . $i);
     file_put_contents($filename, $contents, FILE_APPEND);
     echo 'Done with request number ' . $i . "\n";
     if ($i % 10 == 0) {
-        clean($filename);
+        fixJson($filename);
     }
     $i++;
+}
+fixJson($filename);
+
+function fixJson($filename) {
+    $contents = '[' . file_get_contents($filename) . ']';
+    $contents = str_replace('[[', '[', $contents);
+    $contents = str_replace("},\n]", '}]', $contents);
+    $contents = str_replace('],[', ',', $contents);
+    $contents = str_replace('][', '', $contents);
+    $contents = str_replace('}{', '},{', $contents);
+    $contents = str_replace(']]', ']', $contents);
+    $contents = str_replace('][', ',', $contents);
+    $contents = str_replace('},{', "},\n{", $contents);
+
+    file_put_contents($filename, $contents);
 }
